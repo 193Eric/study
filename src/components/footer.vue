6fc6652036c47fc9aaf43538c4708d14c471ef5f
kaili <373986529@qq.com>
2017-03-31 14:37:28 +0800

    增加笑话和图片

---
+++ src/components/footer.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="nav-box">
+        <p class="nav-list">
+            <router-link class="nav-item" to="/">首页</router-link>
+            <router-link class="nav-item" to="/joke">笑话</router-link>
+            <router-link class="nav-item" to="/joke-img">图片</router-link>
+        </p>
+</div>
+</template>
+<style lang='less'>
+    .nav-box{
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        .nav-list{
+            width:100%;
+            display: flex;
+            .nav-item{
+                flex:1;
+                color: #dae925;
+                font-size: 20px;
+                background:#000;
+                line-height:40px;
+                text-align:center;
+                display:block;
+                font-family: cursive;
+                text-decoration:none;
+            }
+        }
+    }
+</style>     
