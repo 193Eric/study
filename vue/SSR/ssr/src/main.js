fc82e18308dfe853c4d48f71829b83218b91b7ac
193Eric <373986529@qq.com>
2019-12-16 17:47:03 +0800

    update

--- vue/SSR/ssr/src/main.js
+++ vue/SSR/ssr/src/main.js
@@ -2,14 +2,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
-import { createStore } from './store/store.js'
+import { createStore } from './store'
 import { sync } from 'vuex-router-sync'
 
 export function createApp () {
   // 创建 router 实例
   const router = createRouter()
   const store = createStore()
-
+  
   // 同步路由状态(route state)到 store
   sync(store, router)
 
