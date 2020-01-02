1f49d9cb5ee36ab7890ae8d11f54bee2b2c6d585
193Eric <373986529@qq.com>
2020-01-02 11:45:25 +0800

    修改srr bug路径

--- vue/SSR/ssr/src/main.js
+++ vue/SSR/ssr/src/main.js
@@ -2,7 +2,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
-import { createStore } from './store'
+import { createStore } from './store/store'
 import { sync } from 'vuex-router-sync'
 
 export function createApp () {
